21cdad46e7c597f34ece5c57d2a81b4045045e6c
Jared <jared@adadevelopersacademy.org>
2021-02-04 16:03:37 -0800

    add returns to condition_in_words

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -5,17 +5,17 @@ class Item:
 
   def condition_in_words(self):
     if self.condition < 1:
-      "You probably want a glove for this one..."
+      return "You probably want a glove for this one..."
     elif self.condition < 2:
-      "Hopefully, its ok if this doesn't last you unto next month"
+      return "Hopefully, its ok if this doesn't last you unto next month"
     elif self.condition < 3:
-      "It may not be pretty but its sturdy!"
+      return "It may not be pretty but its sturdy!"
     elif self.condition < 4:
-      "Its clearly used...but it could be much worse!"
+      return "Its clearly used...but it could be much worse!"
     elif self.condition < 5:
-      "Almost like its brand new!"
+      return "Almost like its brand new!"
     else:
-      "MINT: Literally hasn't left the box!"
+      return "MINT: Literally hasn't left the box!"
 
   def __str__(self):
     return "Hello World!"
