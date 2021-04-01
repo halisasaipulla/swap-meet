9104a5e4182c5d5aef104d2719bc6dd017c49125
lisa1501 <halisasaipulla@gmail.com>
2021-04-01 00:19:45 -0700

    wave_03_tests_all_passing

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,7 @@
 class Vendor:
       def __init__(self, inventory=[]):
             self.inventory = inventory
-          
+      
       def add(self, item):
             self.inventory.append(item)
             return item
@@ -23,11 +23,14 @@ class Vendor:
       def swap_items(self, friend_list, item_1, item_2):
             if item_1 in self.inventory and item_2 in friend_list.inventory:
                   self.inventory.remove(item_1)
-                  friend_list.append(item_1)
-                  friend_list.remove(item_2)
+                  friend_list.inventory.append(item_1)
+                  friend_list.inventory.remove(item_2)
                   self.inventory.append(item_2)
                   return True
             return False
+
+      # def swap_first_item(self,friend_list):
+      #       if self.inventory!=0 and 
 
                   
             
